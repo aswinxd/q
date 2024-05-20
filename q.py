7f97e4d4e4732dc8ef068d1a418473919ce34c59
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-05-20 13:49:50 +0530

    Update q.py

--- q.py
+++ q.py
@@ -1,6 +1,8 @@
 
 from pyrogram import Client, filters
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
 
